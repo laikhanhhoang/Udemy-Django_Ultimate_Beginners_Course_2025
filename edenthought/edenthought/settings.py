cb21330b3c3793cdecaa90c3156abfcb990151ec
Lai Khanh Hoang <laikhanhhoang3011@gmail.com>
2026-01-29 23:12:54 +0700

    Update settings.py

--- edenthought/edenthought/settings.py
+++ edenthought/edenthought/settings.py
@@ -18,9 +18,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS')
+# CSRF_TRUSTED_ORIGINS = 
 
 
 # Application definition
